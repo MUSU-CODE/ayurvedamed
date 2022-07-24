22e9ac6ad68f8259929bee58aa146746b181d935
MUSU-CODE <mousumitripathy.1.r.d@gmail.com>
2022-07-24 19:37:55 +0530

    rdqts

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -4,14 +4,12 @@ const link = 'http://localhost:8081';
 
 class OrderService {
     createOrder(medicine, customer) {
-        var today = new Date();
-        var od=today.toLocaleDateString();
-        var numberOfDaysToAdd = 6;
-        var result = today.setDate(today.getDate() + numberOfDaysToAdd);
-        var dd=today.toLocaleDateString();
+        // var today = new Date();
+        // var od=today.toLocaleDateString();
+        // var numberOfDaysToAdd = 6;
+        // var result = today.setDate(today.getDate() + numberOfDaysToAdd);
+        // var dd=today.toLocaleDateString();
         var data = {
-            orderDate: od,
-            dispatchDate: dd,
             totalCost: medicine.medicineCost,
             userId: customer.userId,
             medicineList: [{
